f705e422c404a4f9e6ebe4352238597334ae0357
jcmtallon <juanca_mt@hotmail.com>
2022-02-01 23:52:15 +0100

    feat (styles): install tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React, { useEffect } from 'react'
 import { getDocument } from './services/firebase/firestore'
+import './index.css'
 
 const App = function () {
   useEffect(() => {
     getDocument()
   }, [])
 
-  return <div className="App">Hello World</div>
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
 }
 
 export default App
