dd8ac03ba275a7508042414644cf74ba8f0812f6
jcmtallon <juanca_mt@hotmail.com>
2022-02-01 23:58:54 +0100

    feat(ui): install flowbite

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,16 @@ const App = function () {
     getDocument()
   }, [])
 
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button
+        type="button"
+        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        Default
+      </button>
+    </div>
+  )
 }
 
 export default App
