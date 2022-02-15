cea04c2b300867875cb54ab1d38d2d8f5cc897d8
jcmtallon <juanca_mt@hotmail.com>
2022-02-16 00:28:12 +0100

    feat(fb): fetch athelete data

--- src/services/firebase/firestore.ts
+++ src/services/firebase/firestore.ts
@@ -3,14 +3,22 @@ import { app } from './firebaseSetup'
 
 const firestore = getFirestore(app)
 
-const getDocument = async () => {
+const getMark = async () => {
   const docuRef = doc(firestore, `marks/xH5jSxsA2p69HX5xkvPE`)
 
   const response = await getDoc(docuRef)
   const data = response.data()
-  console.log(data)
 
-  return response.data
+  return data
 }
 
-export { getDocument }
+const getAthlete = async () => {
+  const docuRef = doc(firestore, `athletes/XH93p7kZ0ycTeSmsaS7R`)
+
+  const response = await getDoc(docuRef)
+  const data = response.data()
+
+  return data
+}
+
+export { getMark, getAthlete }
--- src/views/dashboard/Dashboard.tsx
+++ src/views/dashboard/Dashboard.tsx
@@ -1,15 +1,32 @@
-import React, { useEffect } from 'react'
-import { getMark, getAthelete } from '../../services/firebase/firestore'
+import React, { useEffect, useState } from 'react'
+import { getAthlete } from '../../services/firebase/firestore'
 
 const Dashboard = function () {
+  const [tempState, setTempState] = useState<any>(null)
+
   useEffect(() => {
-    getAthelete()
-    getMark()
+    const fetchData = async () => {
+      const athlete = await getAthlete()
+      setTempState(athlete)
+    }
+
+    fetchData()
+    // getMark()
   }, [])
 
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      {tempState && (
+        <div>
+          <div>{tempState.name}</div>
+          <div>{tempState.license}</div>
+          <div>{tempState.category}</div>
+          <div>{tempState.club}</div>
+          <div>{tempState.province}</div>
+          <div>{tempState.surnames}</div>
+        </div>
+      )}
       <button
         type="button"
         className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
