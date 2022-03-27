7fa9903d4105994b1693111418ebd69de363f2e3
jcmtallon <juanca_mt@hotmail.com>
2022-03-27 15:15:57 +0200

    feat(theme): add color palette

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -2,3 +2,6 @@ export { ThemeProvider, useTheme } from './ThemeProvider'
 export { typo } from './typography'
 export { GlobalStyles } from './GlobalStyles'
 export { theme } from './theme'
+
+// TODO: remove this testing export
+export { lightPalette } from './color/palettes/lightPalette'
--- src/views/dashboard/views/athletes/Athletes.tsx
+++ src/views/dashboard/views/athletes/Athletes.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
-import { Button } from 'src/components'
+import { lightPalette } from 'theme'
+// import { Button } from 'src/components/src/Button/Button'
 import { getAthlete } from '../../../../services/firebase/firestore'
 import * as S from './Athletes.styles'
 
@@ -57,7 +58,82 @@ const Athletes = function () {
       <S.Sm>Typography (sm)</S.Sm>
       <S.Xs>Typography (xs)</S.Xs>
       <S.Xxs>Typography (xxs)</S.Xxs>
-      <Button>TEST</Button>
+      {/* <Button>TEST</Button> */}
+      <div style={{ backgroundColor: lightPalette.grey[15] }}>grey15</div>
+      <div style={{ backgroundColor: lightPalette.grey[20] }}>grey20</div>
+      <div style={{ backgroundColor: lightPalette.grey[25] }}>grey25</div>
+      <div style={{ backgroundColor: lightPalette.grey[30] }}>grey30</div>
+      <div style={{ backgroundColor: lightPalette.grey[35] }}>grey35</div>
+      <div style={{ backgroundColor: lightPalette.grey[40] }}>grey40</div>
+      <div style={{ backgroundColor: lightPalette.grey[45] }}>grey45</div>
+      <div style={{ backgroundColor: lightPalette.grey[50] }}>grey50</div>
+      <div style={{ backgroundColor: lightPalette.grey[55] }}>grey55</div>
+      <div style={{ backgroundColor: lightPalette.grey[60] }}>grey60</div>
+      <div style={{ backgroundColor: lightPalette.grey[65] }}>grey65</div>
+      <div style={{ backgroundColor: lightPalette.grey[70] }}>grey70</div>
+      <div style={{ backgroundColor: lightPalette.grey[75] }}>grey75</div>
+      <div style={{ backgroundColor: lightPalette.grey[80] }}>grey80</div>
+      <div style={{ backgroundColor: lightPalette.grey[85] }}>grey85</div>
+      <div style={{ backgroundColor: lightPalette.yellow[15] }}>yellow15</div>
+      <div style={{ backgroundColor: lightPalette.yellow[20] }}>yellow20</div>
+      <div style={{ backgroundColor: lightPalette.yellow[25] }}>yellow25</div>
+      <div style={{ backgroundColor: lightPalette.yellow[30] }}>yellow30</div>
+      <div style={{ backgroundColor: lightPalette.yellow[35] }}>yellow35</div>
+      <div style={{ backgroundColor: lightPalette.yellow[40] }}>yellow40</div>
+      <div style={{ backgroundColor: lightPalette.yellow[45] }}>yellow45</div>
+      <div style={{ backgroundColor: lightPalette.yellow[50] }}>yellow50</div>
+      <div style={{ backgroundColor: lightPalette.yellow[55] }}>yellow55</div>
+      <div style={{ backgroundColor: lightPalette.yellow[60] }}>yellow60</div>
+      <div style={{ backgroundColor: lightPalette.yellow[65] }}>yellow65</div>
+      <div style={{ backgroundColor: lightPalette.yellow[70] }}>yellow70</div>
+      <div style={{ backgroundColor: lightPalette.yellow[75] }}>yellow75</div>
+      <div style={{ backgroundColor: lightPalette.yellow[80] }}>yellow80</div>
+      <div style={{ backgroundColor: lightPalette.yellow[85] }}>yellow85</div>
+      <div style={{ backgroundColor: lightPalette.green[15] }}>green15</div>
+      <div style={{ backgroundColor: lightPalette.green[20] }}>green20</div>
+      <div style={{ backgroundColor: lightPalette.green[25] }}>green25</div>
+      <div style={{ backgroundColor: lightPalette.green[30] }}>green30</div>
+      <div style={{ backgroundColor: lightPalette.green[35] }}>green35</div>
+      <div style={{ backgroundColor: lightPalette.green[40] }}>green40</div>
+      <div style={{ backgroundColor: lightPalette.green[45] }}>green45</div>
+      <div style={{ backgroundColor: lightPalette.green[50] }}>green50</div>
+      <div style={{ backgroundColor: lightPalette.green[55] }}>green55</div>
+      <div style={{ backgroundColor: lightPalette.green[60] }}>green60</div>
+      <div style={{ backgroundColor: lightPalette.green[65] }}>green65</div>
+      <div style={{ backgroundColor: lightPalette.green[70] }}>green70</div>
+      <div style={{ backgroundColor: lightPalette.green[75] }}>green75</div>
+      <div style={{ backgroundColor: lightPalette.green[80] }}>green80</div>
+      <div style={{ backgroundColor: lightPalette.green[85] }}>green85</div>
+      <div style={{ backgroundColor: lightPalette.red[15] }}>red15</div>
+      <div style={{ backgroundColor: lightPalette.red[20] }}>red20</div>
+      <div style={{ backgroundColor: lightPalette.red[25] }}>red25</div>
+      <div style={{ backgroundColor: lightPalette.red[30] }}>red30</div>
+      <div style={{ backgroundColor: lightPalette.red[35] }}>red35</div>
+      <div style={{ backgroundColor: lightPalette.red[40] }}>red40</div>
+      <div style={{ backgroundColor: lightPalette.red[45] }}>red45</div>
+      <div style={{ backgroundColor: lightPalette.red[50] }}>red50</div>
+      <div style={{ backgroundColor: lightPalette.red[55] }}>red55</div>
+      <div style={{ backgroundColor: lightPalette.red[60] }}>red60</div>
+      <div style={{ backgroundColor: lightPalette.red[65] }}>red65</div>
+      <div style={{ backgroundColor: lightPalette.red[70] }}>red70</div>
+      <div style={{ backgroundColor: lightPalette.red[75] }}>red75</div>
+      <div style={{ backgroundColor: lightPalette.red[80] }}>red80</div>
+      <div style={{ backgroundColor: lightPalette.red[85] }}>red85</div>
+      <div style={{ backgroundColor: lightPalette.blue[15] }}>blue15</div>
+      <div style={{ backgroundColor: lightPalette.blue[20] }}>blue20</div>
+      <div style={{ backgroundColor: lightPalette.blue[25] }}>blue25</div>
+      <div style={{ backgroundColor: lightPalette.blue[30] }}>blue30</div>
+      <div style={{ backgroundColor: lightPalette.blue[35] }}>blue35</div>
+      <div style={{ backgroundColor: lightPalette.blue[40] }}>blue40</div>
+      <div style={{ backgroundColor: lightPalette.blue[45] }}>blue45</div>
+      <div style={{ backgroundColor: lightPalette.blue[50] }}>blue50</div>
+      <div style={{ backgroundColor: lightPalette.blue[55] }}>blue55</div>
+      <div style={{ backgroundColor: lightPalette.blue[60] }}>blue60</div>
+      <div style={{ backgroundColor: lightPalette.blue[65] }}>blue65</div>
+      <div style={{ backgroundColor: lightPalette.blue[70] }}>blue70</div>
+      <div style={{ backgroundColor: lightPalette.blue[75] }}>blue75</div>
+      <div style={{ backgroundColor: lightPalette.blue[80] }}>blue80</div>
+      <div style={{ backgroundColor: lightPalette.blue[85] }}>blue85</div>
     </S.Wrapper>
   )
 }
